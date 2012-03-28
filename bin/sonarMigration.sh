0b951908a9c170a015caf4f0b02336a9a2d56496
carlosg <carlosg@softwaresano.com>
2012-03-28 18:52:44 +0200

    [MINOR] Remove sonar-ldap1.1

--- bin/sonarMigration.sh
+++ bin/sonarMigration.sh
@@ -27,19 +27,25 @@ sed -i s:"\${sonar\.jdbc\.password}":"${sonar_jdbc_password}":g $TARGET_DIR/bin/
 sed -i s:"\${projectName}":"${projectName}":g $TARGET_DIR/bin/installSonarDB.sh
 sed -i s:"\${sonar\.user}":"${sonar_user}":g $TARGET_DIR/bin/installSonarDB.sh
 sed -i s:"\. \./setEnv\.sh":"\. $PROJECT_HOME_DEVELENV/bin/setEnv\.sh":g $TARGET_DIR/bin/installSonarDB.sh
-chmod u+x $TARGET_DIR/bin/installSonarDB.sh
-echo $PROJECT_HOME_DEVELENV
-exit 0
-
-
-exit 0
+sed -i s:"sonar\.jdbc\.password\:.*":"sonar\.jdbc\.password\:                       develenv":g $TARGET_DIR/app/sonar/conf/sonar.properties
 /etc/init.d/develenv stop
-rm -Rf $PROJECT_HOME_DEVELENV/platform/tomcat/webapps/sonar
-rm -Rf $PROJECT_HOME_DEVELENV/platform/tomcat/conf/Catalina/*
-rm -Rf $PROJECT_HOME_DEVELENV/platform/tomcat/work/*
-rm -Rf $PROJECT_HOME_DEVELENV/platform/tomcat/temp/*
-rm -Rf $PROJECT_HOME_DEVELENV/platform/tomcat/logs/*
-rm -Rf $PROJECT_HOME_DEVELENV/app/sonar
+sleep 10
+su develenv -c "rm -Rf $PROJECT_HOME_DEVELENV/platform/tomcat/webapps/sonar"
+su develenv -c "rm -Rf $PROJECT_HOME_DEVELENV/platform/tomcat/conf/Catalina/*"
+su develenv -c "rm -Rf $PROJECT_HOME_DEVELENV/platform/tomcat/work/*"
+su develenv -c "rm -Rf $PROJECT_HOME_DEVELENV/platform/tomcat/temp/*"
+su develenv -c "rm -Rf $PROJECT_HOME_DEVELENV/platform/tomcat/logs/*"
+su develenv -c "rm -Rf $PROJECT_HOME_DEVELENV/app/sonar"
+su develenv -c "cp -R $TARGET_DIR/platform/tomcat/webapps/sonar $PROJECT_HOME_DEVELENV/platform/tomcat/webapps"
+su develenv -c "cp -R $TARGET_DIR/app/sonar $PROJECT_HOME_DEVELENV/app"
+su develenv -c "cp -R $TARGET_DIR/bin/installSonarDB.sh $PROJECT_HOME_DEVELENV/bin"
+cd $PROJECT_HOME_DEVELENV/bin
+chmod 755 ./installSonarDB.sh
+./installSonarDB.sh
+/etc/init.d/develenv start
+rm -Rf installSonarDB.sh
+rm -Rf $TARGET_DIR
+
 
 
 
